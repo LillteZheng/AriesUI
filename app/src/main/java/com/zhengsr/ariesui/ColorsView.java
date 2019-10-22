45da4443d58d832600f945f5150ea01674d71c82
LillteZheng <845731923@qq.com>
2019-10-22 14:34:24 +0800

    颜色选择 - 增加颜色版测试类

--- app/src/main/java/com/zhengsr/ariesui/ColorsView.java
+++ app/src/main/java/com/zhengsr/ariesui/ColorsView.java
@@ -2,6 +2,11 @@ package com.zhengsr.ariesui;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,6 +16,10 @@ import android.view.View;
  * Describe:
  */
 public class ColorsView extends View {
+
+    private Paint paint;
+    private Rect rect;
+
     public ColorsView(Context context) {
         this(context,null);
     }
@@ -26,59 +35,22 @@ public class ColorsView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-      //  int[] colors = new int[]
-        /*if (BORDER_WIDTH_PX > 0) {
-            borderPaint.setColor(borderColor);
-
-            canvas.drawRect(rect.left - BORDER_WIDTH_PX,
-                    rect.top - BORDER_WIDTH_PX,
-                    rect.right + BORDER_WIDTH_PX,
-                    rect.bottom + BORDER_WIDTH_PX,
-                    borderPaint);
+        rect = new Rect(0,0,w,h);
+        int[] hue = new int[361];
+        int count = 0;
+        for (int i = hue.length - 1; i >= 0; i--,count++) {
+            hue[count] = Color.HSVToColor(new float[]{i,1f,1f});
         }
+        LinearGradient linearGradient = new LinearGradient(rect.left, rect.top,
+                rect.left, rect.bottom,hue,null, Shader.TileMode.CLAMP);
+        paint = new Paint();
+        paint.setShader(linearGradient);
 
-        if (hueBackgroundCache == null) {
-            hueBackgroundCache = new BitmapCache();
-            hueBackgroundCache.bitmap = Bitmap.createBitmap(rect.width(), rect.height(), Config.ARGB_8888);
-            hueBackgroundCache.canvas = new Canvas(hueBackgroundCache.bitmap);
-
-            int[] hueColors = new int[(int) (rect.height() + 0.5f)];
-
-            // Generate array of all colors, will be drawn as individual lines.
-            float h = 360f;
-            for (int i = 0; i < hueColors.length; i++) {
-                hueColors[i] = Color.HSVToColor(new float[]{h, 1f, 1f});
-                h -= 360f / hueColors.length;
-            }
-
-            // Time to draw the hue color gradient,
-            // its drawn as individual lines which
-            // will be quite many when the resolution is high
-            // and/or the panel is large.
-            Paint linePaint = new Paint();
-            linePaint.setStrokeWidth(0);
-            for (int i = 0; i < hueColors.length; i++) {
-                linePaint.setColor(hueColors[i]);
-                hueBackgroundCache.canvas.drawLine(0, i, hueBackgroundCache.bitmap.getWidth(), i, linePaint);
-            }
-        }
-
-        canvas.drawBitmap(hueBackgroundCache.bitmap, null, rect, null);
-
-        Point p = hueToPoint(hue);
-
-        RectF r = new RectF();
-        r.left = rect.left - sliderTrackerOffsetPx;
-        r.right = rect.right + sliderTrackerOffsetPx;
-        r.top = p.y - (sliderTrackerSizePx / 2);
-        r.bottom = p.y + (sliderTrackerSizePx / 2);
-
-        canvas.drawRoundRect(r, 2, 2, hueAlphaTrackerPaint);*/
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        canvas.drawRect(rect,paint);
     }
 }
